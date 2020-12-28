52c983c200c9b24a59ff5228eed7fb1be1cef90a
dmytrolutsyk <dmytro@LAPTOP-R5T7TI5S.localdomain>
2020-12-28 21:32:39 +0100

    add point class

---
+++ Point.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Dima on 28/12/2020.
+//
+
+#include "Point.h"
+Point::Point(){
+    this-> m_x = 1;
+    this-> m_y = 1;
+}
+
+void Point::setX(float& x)
+{
+    this->m_x = x;
+}
+
+void Point::setY(float& y)
+{
+    this->m_y = y;
+}
+
+float Point::getX()
+{
+    return this->m_x;
+}
+
+float Point::getY() {
+    return this->m_y;
+}
+
+void Point::add(Point Point){
+    this->m_x = m_x + Point.m_x;
+    this->m_y = m_y + Point.m_y;
+}
